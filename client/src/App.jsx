3556f0aa14e5dde1a7bb531538ef1b887820cddc
Prasid <prasidpoudelsharma@gmail.com>
2025-06-10 22:11:35 -0400

    test1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -23,7 +23,7 @@ const App = () => {
     const fetchUser = async () => {
       try {
         const response = await axios.get(
-          "http://localhost:4000/api/v1/user/getuser",
+          "https://jobserver-l1v3oqimz-sharmaprasids-projects.vercel.app/api/v1/user/getuser",
           {
             withCredentials: true,
           }
