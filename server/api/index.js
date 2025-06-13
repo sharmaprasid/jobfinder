622f191b3527e3bc64352fee03c4afed81f1f5f6
Prasid <prasidpoudelsharma@gmail.com>
2025-06-12 21:03:33 -0400

    index.js updated

--- server/api/index.js
+++ server/api/index.js
@@ -1,3 +1,18 @@
-import app from '../app.js';
+// api/index.js
+import app from "../app.js";
+import { createServer } from "http";
 
-export default app;
+export default async function handler(req, res) {
+  if (req.method === "OPTIONS") {
+    
+    res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+    res.status(200).end();
+    return;
+  }
+
+  const server = createServer(app);
+  server.emit("request", req, res);
+}
