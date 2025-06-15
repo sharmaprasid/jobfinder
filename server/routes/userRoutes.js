7f50eea6eb6654950ef1d331b3cf6d2ae5cb0174
Prasid <prasidpoudelsharma@gmail.com>
2025-06-15 15:42:45 -0400

    index.js updateds232

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -3,10 +3,8 @@ import { login, register, logout, getUser } from "../controllers/userController.
 import { isAuthenticated } from "../middlewares/auth.js";
 
 const router = express.Router();
-
 router.post("/register", register);
 router.post("/login", login);
 router.get("/logout", isAuthenticated, logout);
 router.get("/getuser", isAuthenticated, getUser);
-
 export default router;
