a1d34dc8891a325d248cc2613a5e395132a5ec0f
Prasid <prasidpoudelsharma@gmail.com>
2025-06-15 20:05:50 -0400

    index.js updateds2222

--- server/utils/jwtToken.js
+++ server/utils/jwtToken.js
@@ -1,10 +1,13 @@
 export const sendToken = (user, statusCode, res, message) => {
   const token = user.getJWTToken();
+
   const options = {
     expires: new Date(
       Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
     ),
-    httpOnly: true, // Set httpOnly to true
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production", // only secure in prod
+    sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax", // required for cross-site cookies
   };
 
   res.status(statusCode).cookie("token", token, options).json({
