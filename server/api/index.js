ff6f9066991d6f5876dbc69f788b89985f92af75
Prasid <prasidpoudelsharma@gmail.com>
2025-06-21 01:21:57 -0400

    updated

--- server/api/index.js
+++ server/api/index.js
@@ -4,7 +4,7 @@ import { createServer } from "http";
 
 export default async function handler(req, res) {
   // CORS Headers for ALL requests
-  res.setHeader("Access-Control-Allow-Origin", "https://jobfinder-frontend-sharmaprasids-projects.vercel.app");
+  res.setHeader("Access-Control-Allow-Origin", "https://jobfinder-frontend-beta.vercel.app");
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Credentials", "true");
