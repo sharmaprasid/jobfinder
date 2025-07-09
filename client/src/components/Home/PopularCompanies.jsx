3e67f05c7bd12fbc72de234aff86441c56ce086f
Prasid <prasidpoudelsharma@gmail.com>
2025-07-08 20:11:24 -0400

    prasid123

--- client/src/components/Home/PopularCompanies.jsx
+++ client/src/components/Home/PopularCompanies.jsx
@@ -37,7 +37,7 @@ const PopularCompanies = () => {
   useEffect(() => {
     const fetchCompanies = async () => {
       try {
-        const res = await axios.get(`${import.meta.env.VITE_APP_API_URL}/company`, {
+        const res = await axios.get(`https://jobfinderserver.vercel.app/api/v1/company`, {
           withCredentials: true,
         });
         setCompanies(res.data.companies);
